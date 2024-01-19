6165b56461a6c9e9cae6a752866dc5d0c9fb1387
Nikolas1790 <125148113+Nikolas1790@users.noreply.github.com>
2024-01-19 15:52:51 +0200

    id-car

--- src/components/ModalWindow/ModalWindow.jsx
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -3,37 +3,11 @@ import { ModalWindowBackdrop, ModalWindowContainer, ModalWindowImg, ModelAndYear
 import { useEffect } from "react";
 import { fetchCarById } from "redax/cars/operation";
 import { CarModel } from "components/CatalogList/CatalogList.styled";
-const a = 
-  {
-      "id": 9582,
-      "year": 2008,
-      "make": "Buick",
-      "model": "Enclave",
-      "type": "SUV",
-      "img": "https://s1.1zoom.me/big0/64/Buick_Enclave_Avenir_2021_Crossover_Black_Metallic_599554_1280x693.jpg",
-      "description": "The Buick Enclave is a stylish and spacious SUV known for its comfortable ride and luxurious features.",
-      "fuelConsumption": "10.5",
-      "engineSize": "3.6L V6",
-      "accessories": [
-          "Leather seats",
-          "Panoramic sunroof",
-          "Premium audio system"
-      ],
-      "functionalities": [
-          "Power liftgate",
-          "Remote start",
-          "Blind-spot monitoring"
-      ],
-      "rentalPrice": "$40",
-      "rentalCompany": "Luxury Car Rentals",
-      "address": " Kiev | Ukraine",
-      "rentalConditions": "Minimum age: 25\nValid driver's license\nSecurity deposit required",
-      "mileage": 5858
-  }
+
 export default function ModalWindow({ carId }) {
   const dispatch = useDispatch();
-  // const selectedCar = useSelector((state) => state.cars.selectedCar);
-console.log('kkkkkkkkkkkkkkkkk',a.year)
+  const selectedCar = useSelector((state) => state.cars.selectedCar);
+console.log('kkkkkkkkkkkkkkkkk',selectedCar)
 
   useEffect(() => {
     dispatch(fetchCarById(carId))
@@ -43,8 +17,8 @@ console.log('kkkkkkkkkkkkkkkkk',a.year)
     return (
       <ModalWindowBackdrop>
       <ModalWindowContainer>
-        <ModalWindowImg alt={a.img} src={a.img}></ModalWindowImg>
-        <ModelAndYearTitle >{a.make} <CarModel>{a.model}</CarModel>, {a.year}</ModelAndYearTitle>
+        <ModalWindowImg alt={selectedCar.img} src={selectedCar.img}></ModalWindowImg>
+        <ModelAndYearTitle >{selectedCar.make} <CarModel>{selectedCar.model}</CarModel>, {selectedCar.year}</ModelAndYearTitle>
       </ModalWindowContainer>
     </ModalWindowBackdrop>
     );
